d5ec601ba680f726b991c31abcf0249bc0bbeda1
DimitraK92 <degraded_rebuilt@tutanota.com>
2024-09-05 10:25:31 +0300

    higher-lower game code

---
+++ helper.py
@@ -0,0 +1,19 @@
+﻿import pyfiglet
+
+def validate_user_input_play_again(user_input):
+    while user_input.lower().strip() not in ["y", "n"]:
+        user_input = input("Wrong input. Try again: ").lower().strip()
+    return user_input.lower()
+
+def get_person_details(data_item):
+    return f"{data_item.get('name')}, a {data_item.get('description')}, from {data_item.get('country')}."
+
+def validate_user_input_compare(user_input):
+    while user_input.upper().strip() not in ["A", "B"]:
+        user_input = input("Wrong input. Try again: ").upper().strip()
+    return user_input.upper().strip()
+
+def print_compare(data_item1, data_item2):
+    print("Compare A: " + get_person_details(data_item1))
+    print(pyfiglet.figlet_format("VS"))
+    print("Against B: " + get_person_details(data_item2))
---
+++ main.py
@@ -0,0 +1,40 @@
+﻿import pyfiglet
+import random
+import signal
+from os import system
+from game_data import data
+import helper
+
+def handler(signum, frame):
+    system("cls")
+    exit(1)
+
+signal.signal(signal.SIGINT, handler)
+
+def start_game():
+    print("Welcome to the Higher Lower Game!")
+    play_again = True
+    while play_again:
+        play_again = play_game()
+        system("cls")
+    system("cls")
+
+def play_game():
+    print(pyfiglet.figlet_format("Higher Lower"))
+    score = 0
+    wrong_choice = False
+    while not wrong_choice:
+        random_figures = random.sample(data, 2)
+        helper.print_compare(random_figures[0], random_figures[1])
+        winner = max(random_figures, key=lambda x: x["follower_count"])
+        correct_choice = 'A' if random_figures[0] == winner else 'B'
+        user_choice = helper.validate_user_input_compare(input("Who has more followers on Instagram? A/B: "))
+        if user_choice == correct_choice:
+            score += 1
+            print(f"Correct! Current score: {score}")
+        else:
+            wrong_choice = True
+            print(f"Wrong choice. Final score: {score}")
+    return helper.validate_user_input_play_again(input("Do you want to play again? (y/n): ")) == 'y'
+
+start_game()
